acdc92b23e3f7cca26b6f61d3ae041ef46db6443
ashokwaghmare <ashok.waghmare.cse@gmail.com>
2017-04-07 18:22:24 +0530

    first commit

---
+++ app/controllers/AsyncController.scala
@@ -0,0 +1,54 @@
+package controllers
+
+import akka.actor.ActorSystem
+import javax.inject._
+import play.api._
+import play.api.mvc._
+import scala.concurrent.{ExecutionContext, Future, Promise}
+import scala.concurrent.duration._
+import play.api.libs.ws._
+/**
+ * This controller creates an `Action` that demonstrates how to write
+ * simple asynchronous code in a controller. It uses a timer to
+ * asynchronously delay sending a response for 1 second.
+ *
+ * @param actorSystem We need the `ActorSystem`'s `Scheduler` to
+ * run code after a delay.
+ * @param exec We need an `ExecutionContext` to execute our
+ * asynchronous code.
+ */
+@Singleton
+class AsyncController @Inject() (actorSystem: ActorSystem, ws: WSClient)(implicit exec: ExecutionContext) extends Controller {
+
+  /**
+   * Create an Action that returns a plain text message after a delay
+   * of 1 second.
+   *
+   * The configuration in the `routes` file means that this method
+   * will be called when the application receives a `GET` request with
+   * a path of `/message`.
+   */
+  def message = Action.async {
+    ws.url("http://timesofindia.indiatimes.com/rssfeeds/1221656.cms").get.map{
+
+      i => 
+      val m = scala.xml.XML.loadString(i.body)
+      val item = m \\ "item"
+      val maharashtra = item.filter(i => (i \\ "description").text.contains("Pakistan"))
+      val link = maharashtra \\ "link"
+      //Ok(<r>{link.text}</r>)
+      Redirect(link.text)
+
+    }
+    //getFutureMessage(1.second).map { msg => Ok(msg) }
+  }
+
+  private def getFutureMessage(delayTime: FiniteDuration): Future[String] = {
+    val promise: Promise[String] = Promise[String]()
+    actorSystem.scheduler.scheduleOnce(delayTime) {
+     promise.success("Hi!") 
+   }
+    promise.future
+  }
+
+}
